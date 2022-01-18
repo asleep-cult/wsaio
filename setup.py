312d19818e0fca066f7ea967b3a45bfcddd423c2
asleep-cult <asleep.cult@gmail.com>
2022-01-18 11:05:49 -0500

    Fixed .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fp:
 
 setuptools.setup(
     name='wsaio',
-    version='0.1.1',
+    version='0.1.2',
     author='asleep-cult',
     description='An event-driven WebSocket library for Python.',
     long_description=readme,
